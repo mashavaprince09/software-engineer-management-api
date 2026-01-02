607058d5865d9b02dc2204a6959194648f13fe63
Prince Mashava <mashavaprince09@gmail.com>
2026-01-02 19:14:08 +0200

    Added repository, service and controller for SoftwareEngineer entity

---
+++ src/main/java/com/amigoscode/SoftwareEngineeerRepository.java
@@ -0,0 +1,7 @@
+package com.amigoscode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineeerRepository extends JpaRepository <SoftwareEngineer, Integer> {
+
+}
