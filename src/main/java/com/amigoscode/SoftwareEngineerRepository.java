f3411c732667f648b3cdb8e58cd201dca6c1ee17
Prince Mashava <mashavaprince09@gmail.com>
2026-01-08 23:07:41 +0200

    Added comment for extending JpaRepository

--- src/main/java/com/amigoscode/SoftwareEngineerRepository.java
+++ src/main/java/com/amigoscode/SoftwareEngineerRepository.java
@@ -2,6 +2,7 @@ package com.amigoscode;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//                                                                <Entity Class, Primary Key Type>
 public interface SoftwareEngineerRepository extends JpaRepository <SoftwareEngineer, Integer> {
 
 }
